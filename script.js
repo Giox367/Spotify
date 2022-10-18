fe1234d4986961788fbad45e9c0d4d53bd772ef1
Luigi Indaco <luigindaco@gmail.com>
2022-10-18 17:37:31 +0200

    sidebar rigth completa

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.getElementById("sidebar-right").style.width = "200px";
+  document.getElementById("sidebar-right").style.width = "220px";
 }
 
 function closeNav() {
