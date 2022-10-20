f9a68976a0432def68c48113049d1a1916559c19
simp1408 <simone.ponzani@libero.it>
2022-10-20 10:08:21 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("sidebar-right").style.width = "220px";
+}
+
+function closeNav() {
+  document.getElementById("sidebar-right").style.width = "0";
+}
+function dropDotMenu() {
+  document.getElementById("dotMenu").classList.toggle("show");
+}
+
+
+function openSubMenu() {
+  document.getElementById("sub-list").classList.toggle("show");
+}
